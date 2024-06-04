413d15c73472e8f6bf40249bf7f10227e05d20ab
AValenzue <zuellagraphy@gmail.com>
2024-06-04 22:27:59 +0200

    fix error mongo uri

--- db/index.js
+++ db/index.js
@@ -5,9 +5,9 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://avzkitchen:h*3GGfP*7sENgVy@dokoro.yadqwfg.mongodb.net/";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://avzkitchen:DOg7i83ibj9Gx1hw@dokoro.yadqwfg.mongodb.net/";
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
