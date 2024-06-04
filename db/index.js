6ed7f47491431968d882234e6acece1d96766935
AValenzue <zuellagraphy@gmail.com>
2024-06-04 22:13:56 +0200

    install mongoose

--- db/index.js
+++ db/index.js
@@ -5,10 +5,10 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/dokoro-server";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://avzkitchen:h*3GGfP*7sENgVy@dokoro.yadqwfg.mongodb.net/";
 
 mongoose
-  .connect(MONGO_URI)
+  .connect(MONGODB_URI)
   .then((x) => {
     const dbName = x.connections[0].name;
     console.log(`Connected to Mongo! Database name: "${dbName}"`);
