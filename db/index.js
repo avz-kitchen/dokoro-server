a2939059c3d0ca4d009da9e3f6cc5ae2a3c4be5d
AValenzue <zuellagraphy@gmail.com>
2024-06-04 22:18:46 +0200

    fix error

--- db/index.js
+++ db/index.js
@@ -5,14 +5,12 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://avzkitchen:h*3GGfP*7sENgVy@dokoro.yadqwfg.mongodb.net/";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb+srv://avzkitchen:h*3GGfP*7sENgVy@dokoro.yadqwfg.mongodb.net/";
 
-mongoose
-  .connect(MONGODB_URI)
-  .then((x) => {
-    const dbName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${dbName}"`);
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
   })
   .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
+    console.error('Error connecting to MongoDB:', err);
   });
