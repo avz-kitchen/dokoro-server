782686223584e9f6da15177c774c1dd9c62459ef
AValenzue <zuellagraphy@gmail.com>
2024-06-05 15:43:06 +0200

    fix error cors

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ require("./config")(app);
 
 // 👇 Start handling routes here
 
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
 
 const authRoutes = require("./routes/auth.routes");
 app.use("/auth", authRoutes);
